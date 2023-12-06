42e1ffcc10663bcb56bebe50384fad5a0c58c0a8
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-12-06 13:51:34 -0300

    Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,10 +35,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); //Ponto A, ponto B, interpolacao
         
+    }
 
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 
 
