4780ffc38a30877783ef681b0d69b0dda54d9919
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2024-01-09 14:02:40 -0300

    Plate Complete Visual

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSO_GameObjectsList;
+
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        foreach (KitchenObjectSO_GameObject kitchenObjectSO_GameObject in kitchenObjectSO_GameObjectsList)
+        {
+            kitchenObjectSO_GameObject.gameObject.SetActive(false); //Comeca desativado
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSO_GameObject in kitchenObjectSO_GameObjectsList)
+        {
+            if (kitchenObjectSO_GameObject.kitchenObjectSO == e.kitchenObjectSO)
+            {
+                kitchenObjectSO_GameObject.gameObject.SetActive(true); //Ativa o gameobject colocado no prato
+            }
+        }
+    }
+}
